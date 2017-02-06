6a1838319749f0e8757483dd63ee62eda55ca57c
Charles Uko <charles4genius@yahoo.com>
2017-02-06 13:10:29 +0100

    added slug to URL routes

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -1,9 +1,14 @@
 @extends('main')
 
-@section('title', '| {{ $post-title }}')
+@section('title', "| $post->title ")
 
 @section('content')
 
-	.row>.col-md-8
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<h1>{{ $post->title }}</h1>
+			<p> {{$post->body}} </p>
+		</div>
+	</div>
 
 @endsection
