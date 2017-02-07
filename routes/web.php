2fcc8787e740c508ff8b3165d2ce0102a5e6d20b
Charles Uko <charles4genius@yahoo.com>
2017-02-07 11:35:09 +0100

    created authentication

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Authentication routes
+Route::get('auth/login', 'Auth\LoginController@showLoginForm');
+Route::post('auth/login', 'Auth\LoginController@login');
+Route::get('auth/logout', 'Auth\LoginController@logout');
+//Registration routes
+Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('auth/register', 'Auth\RegisterController@register');
+
 Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 Route::get('blog', ['uses' =>'BlogController@getIndex', 'as' =>'blog.index']);
 
