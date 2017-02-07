26721aefdafe222502b7f1d0c28c440a77b81fb3
Charles Uko <charles4genius@yahoo.com>
2017-02-07 15:01:54 +0100

    Authentication UX

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 //Authentication routes
-Route::get('auth/login', 'Auth\LoginController@showLoginForm');
+Route::get('auth/login', ['as'=>'login', 'uses'=>'Auth\LoginController@showLoginForm']);
 Route::post('auth/login', 'Auth\LoginController@login');
 Route::get('auth/logout', 'Auth\LoginController@logout');
 //Registration routes
